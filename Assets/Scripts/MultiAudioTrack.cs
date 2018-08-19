ce1bb16dc9c1ca6d1a1eba1bbcff0f8b2843b9df
kiyotakay96 <kiyotakay96@gmail.com>
2018-08-19 19:33:00 +0900

    Projectをコミット

---
+++ Assets/Scripts/MultiAudioTrack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiAudioTrack : MonoBehaviour
+{
+    //[SerializeField]
+    //private AudioClip defaultAudioClio;
+    //[SerializeField]
+    //private AudioSource secondAudio;
+
+    //private AudioSource audioSource;
+    //private AudioClip audioClip;
+    //private AudioClip secondAudioClip;
+
+    //private void Start()
+    //{
+    //    if (audioSource == null)
+    //    {
+    //        audioSource = GetComponent<AudioSource>();
+    //    }
+    //    audioClip = AudioClipExtensions.CreateSpeakerSpecificClip(defaultAudioClio, 2, 1);
+    //    secondAudioClip = AudioClipExtensions.CreateSpeakerSpecificClip(defaultAudioClio, 2, 2);
+    //    PlayAudio();
+    //}
+
+    //private void PlayAudio()
+    //{
+    //    audioSource.clip = audioClip;
+    //    audioSource.Play();
+    //    secondAudio.clip = secondAudioClip;
+    //    secondAudio.Play();
+    //}
+
+    [SerializeField]
+    private List<AudioSource> audioSourceList;
+    [SerializeField]
+    private AudioClip defaultAudioClip;
+    
+
+
+
+    
+
+}
---
+++ Assets/Scripts/PointerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointerController : MonoBehaviour
+{
+    private static readonly float MAX_DISTANCE = 100f;
+    private static readonly float DEFAULT_Z_POS = 10f;
+    private Camera mainCamera;
+    private Vector3 touchScreenPosition;
+    private Vector3 touchWorldPosition;
+    private bool isSelected = false;
+
+    private void Start()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+    }
+
+    private void Update()
+    {
+        MouseInput();
+        UpdateMouseTransform();
+    }
+
+    private void UpdateMouseTransform()
+    {
+        if (isSelected)
+        {
+            touchScreenPosition = Input.mousePosition;
+            touchScreenPosition.x = Mathf.Clamp(touchScreenPosition.x, 0.0f, Screen.width);
+            touchScreenPosition.y = Mathf.Clamp(touchScreenPosition.y, 0.0f, Screen.height);
+            touchScreenPosition.z = DEFAULT_Z_POS;
+            touchWorldPosition = mainCamera.ScreenToWorldPoint(touchScreenPosition);
+            this.transform.position = touchWorldPosition;
+        }
+    }
+
+    private void MouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            InitRaycast();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isSelected = false;
+        }
+    }
+
+    private void InitRaycast()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float maxDistance = MAX_DISTANCE;
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, maxDistance);
+        if (hit.collider)
+        {
+            if (hit.collider.gameObject.name == "Pointer")
+            {
+                isSelected = true;
+            }
+        }
+       
+    }
+}
