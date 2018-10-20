7129aa6403e4ae1993a9d55d1f317c9330530b8f
kiyotakay96 <kiyotakay96@gmail.com>
2018-10-20 12:16:48 +0900

    gui素材ついか

---
+++ Assets/Scripts/Finder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Finder : MonoBehaviour
+{
+    [SerializeField]
+    private List<TextMeshProUGUI> _TestList;
+
+    public void GetTextList()
+    {
+
+    }
+}
